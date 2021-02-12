8bac01e198e761df8114325cd9a82332b8e56d66
matiaswasiak <matiaswasiak@gmail.com>
2021-02-12 12:46:05 -0300

    Primera página

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Lilly"
-    lastName="Kaufman"
-    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
-    jobTitle="Frontend Developer"
-    twitter="matiaswasiak_"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
