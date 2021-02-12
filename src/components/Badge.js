52a7806c4fcbe9138030d1233e815b8cd4b1f8bf
matiaswasiak <matiaswasiak@gmail.com>
2021-02-12 12:00:51 -0300

    Aplicando estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,33 @@
 import React from "react";
 
+import "./styles/Badge.css";
 import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          />
           <h1>
             Matias <br /> Wasiak
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Developer</p>
-          <p>@matiaswasiak</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Developer</h3>
+          <div>@matiaswasiak</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
