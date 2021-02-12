81a31eee3392db50ac6fb358c932820e820e005b
matiaswasiak <matiaswasiak@gmail.com>
2021-02-11 21:35:50 -0300

    JSX

--- src/index.js
+++ src/index.js
@@ -8,7 +8,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzi Badges from React!</h1>;
+// const jsx = <h1>Hello, Platzi Badges from React!</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a Platzi"
+// );
+// const name = "Matias";
+// const element = React.createElement("h1", {}, `Hola, soy ${name}`);
+const jsx = (
+  <div>
+    <h1>Hola, soy Matías!</h1>
+    <p>Soy ingeniero frontend.</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola, soy Matías!"),
+  React.createElement("p", {}, "Soy ingeniero de la web")
+);
 
 const container = document.getElementById("app");
 
