75f796b3cc5fdb49c0bce8dcdc3160e0abb18184
matiaswasiak <matiaswasiak@gmail.com>
2021-02-13 12:13:20 -0300

    Levantamiento del estado

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,28 @@ import BadgeForm from "../components/BadgeForm";
 import Navbar from "../components/Navbar";
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    },
+  };
+
+  handleChange = (e) => {
+    // const nextForm = this.state.form;
+    // nextForm[e.target.name] = e.target.value;
+
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -19,16 +41,20 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col">
               <Badge
-                firstName="Matias"
-                lastName="Wasiak"
-                twitter="matiaswasiak_"
-                jobTitle="Frontend Developer"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://www.gravatar.com/avatar?=identicon"
               />
             </div>
 
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
