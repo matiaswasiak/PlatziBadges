b82e31c7b43f3d48c5f4c599fb4a90ba13b00e86
matiaswasiak <matiaswasiak@gmail.com>
2021-02-16 17:31:54 -0300

    Manejando los estados de la petición durante el POST

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,10 +4,13 @@ import "./styles/BadgeNew.css";
 import header from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
+import PageLoading from "../components/PageLoading";
 import api from "../api";
 
 class BadgeNew extends React.Component {
   state = {
+    loading: false,
+    error: null,
     form: {
       firstName: "",
       lastName: "",
@@ -36,12 +39,17 @@ class BadgeNew extends React.Component {
     try {
       await api.badges.create(this.state.form);
       this.setState({ loading: false });
+
+      this.props.history.push("/badges");
     } catch (error) {
       this.setState({ loading: false, error: error });
     }
   };
 
   render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
@@ -70,6 +78,7 @@ class BadgeNew extends React.Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 formValues={this.state.form}
+                error={this.state.error}
               />
             </div>
           </div>
